81e5033cb498ff0cc50178b32f847024a0e8f66f
Seth Stephens <relative@airmail.cc>
2017-12-29 12:58:51 -0500

    Express errors and custom response

---
+++ Response.js
@@ -0,0 +1,59 @@
+/**
+ * Custocord Registry Backend
+ * 
+ * File...................Response.js
+ * Created on.............Friday, 29th December 2017 12:33:28 pm
+ * Created by.............Relative
+ * 
+ */
+class Response {
+  /**
+   * Construct a new Response
+   * @param {Boolean} [success = true] - Did the request succeed 
+   * @param {String} [message = "OK"] - Message for the meta
+   * @param {Number} [status = 200] - Status code
+   * @param {Object} payload - Payload
+   */
+  constructor(success = true, message, status, payload) {
+    if (typeof success === 'object') {
+      this.success = true
+      this.message = 'OK'
+      this.status = 200
+      this.payload = success
+    }
+    if (typeof message === 'object') {
+      this.message = 'OK'
+      this.status = 200
+      this.payload = message
+    }
+    if (typeof message === 'string') this.message = message
+    if (typeof status === 'object') {
+      this.status = 200
+      this.payload = status
+    }
+    if (typeof payload === 'object') {
+      this.success = success
+      this.status = status
+      this.message = message
+      this.payload = payload
+    }
+    if (typeof this.payload !== 'object') { // Sanity checks
+      this.success = success || true
+      this.status = status || 200
+      this.message = message || 'OK'
+      this.payload = payload || {}
+    }
+    return this.obj()
+  }
+  obj() {
+    const obj = Object.assign({
+      meta: {
+        success: this.success || true,
+        status: this.status || 200,
+        message: this.message || 'OK'
+      }
+    }, this.payload)
+    return obj
+  }
+}
+module.exports = Response
---
+++ errors.js
@@ -0,0 +1,50 @@
+/**
+ * Custocord Registry Backend
+ * 
+ * File...................errors.js
+ * Created on.............Friday, 29th December 2017 12:10:29 pm
+ * Created by.............Relative
+ * 
+ */
+class ServerError extends Error {
+  constructor(message, code, status) {
+    super(message || 'An unknown error occurred, try your request later')
+    this.name = this.constructor.name
+    Error.captureStackTrace(this, this.constructor)
+
+    this.status = status || 500
+    this.code = code || `${this.status}-UNKWN`
+  }
+}
+
+class PackageNotFoundError extends ServerError {
+  constructor(pkg) {
+    super(typeof pkg === 'string' ? `The package "${pkg}" was unable to be found in the database` : 'The package you were looking for was unable to be found in the database', `404-PKGNF-${pkg}`, 404)
+  }
+}
+
+class ParameterNotDefinedError extends ServerError {
+  constructor(param) {
+    super(`The parameter "${param}" was not defined in your request. Define it and try again.`, `400-PUNDEF-${param}`, 400)
+  }
+}
+
+class InvalidTokenError extends ServerError {
+  constructor() {
+    super('Your token is invalid', '403-BADTKN', 403)
+  }
+}
+
+class UnauthorizedError extends ServerError {
+  constructor() {
+    super('You are not allowed to modify that package', '403-UNAUTH', 403)
+  }
+}
+
+module.exports = {
+  ServerError,
+  PackageNotFoundError,
+  ParameterNotDefinedError,
+  InvalidTokenError,
+  UnauthorizedError
+}
